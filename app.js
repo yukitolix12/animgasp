f3624db8db96e2f2d431d7b0addfd1fd8ab590d0
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-26 15:23:05 +0900

    animationに幅の縮小機能を加える

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ const headline = document.querySelector(".headline");
 
 const tl = gsap.timeline();
 
-tl.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut});
+tl.fromTo(
+    hero,
+    1,
+    {height: "0%"},
+    {height: "80%", ease: Power2.easeInOut}
+).fromTo(hero, 1.2, {width: "100%"}, {width: "80%"});
